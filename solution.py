2c29ecf7daba056d042de4ad9b381a78d93298c3
ksanu <j.plebaniak@gmail.com>
2019-03-25 15:12:58 +0100

    Regresja liniowa wielu zmiennych. Kolumny liczbowe. NaN zamienione na -1.

---
+++ solution.py
@@ -0,0 +1,24 @@
+import csv
+import pandas
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+#tylko dane liczbowe
+#dla wartości nan trzeba usunąć/wypełnić
+
+X_train = pandas.read_csv('train/train.tsv',
+                    usecols=["Powierzchnia w m2", "Liczba pokoi", "Liczba pięter w budynku", "Rok budowy" ],
+                    delimiter='\t', encoding="utf-8").fillna(-1)
+
+y_train = pandas.read_csv('train/train.tsv', usecols=["cena"], delimiter='\t', encoding="utf-8").fillna(-1)
+
+X_test_in = pandas.read_csv('test-A/in.tsv', sep='\t', header=None, usecols=[0, 1, 3, 8]).fillna(-1)
+
+reg = LinearRegression().fit(X_train, y_train)
+
+y_out = reg.predict(X_test_in)
+
+with open('test-A/out.tsv', 'w') as output_file:
+    for out in y_out:
+        print('%.0f' % out, file = output_file)
